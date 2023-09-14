5504e2891da525ec805dc9943ed56cc6d9f9e1bd
Matt Topol <zotthewizard@gmail.com>
2023-09-14 11:47:34 -0400

    shift S3 code to s3.go file

--- io/io.go
+++ io/io.go
@@ -18,31 +18,12 @@
 package io
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"io"
 	"io/fs"
-	"net/http"
 	"net/url"
-	"os"
 	"strings"
-
-	"github.com/aws/aws-sdk-go-v2/aws"
-	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/wolfeidau/s3iofs"
-)
-
-const (
-	S3Region          = "s3.region"
-	S3SessionToken    = "s3.session-token"
-	S3SecretAccessKey = "s3.secret-access-key"
-	S3AccessKeyID     = "s3.access-key-id"
-	S3EndpointURL     = "s3.endpoint"
-	S3ProxyURI        = "s3.proxy-uri"
 )
 
 // IO is an interface to a hierarchical file system.
@@ -233,67 +214,7 @@ func inferFileIOFromSchema(path string, props map[string]string) (IO, error) {
 
 	switch parsed.Scheme {
 	case "s3", "s3a", "s3n":
-		opts := []func(*config.LoadOptions) error{}
-		endpoint, ok := props[S3EndpointURL]
-		if !ok {
-			endpoint = os.Getenv("AWS_S3_ENDPOINT")
-		}
-
-		if endpoint != "" {
-			opts = append(opts, config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-				if service != s3.ServiceID {
-					// fallback to default resolution for the service
-					return aws.Endpoint{}, &aws.EndpointNotFoundError{}
-				}
-
-				return aws.Endpoint{
-					URL:               endpoint,
-					SigningRegion:     region,
-					HostnameImmutable: true,
-				}, nil
-			})))
-		}
-
-		if defaultRegion, ok := props[S3Region]; ok {
-			opts = append(opts, config.WithDefaultRegion(defaultRegion))
-		}
-
-		accessKey, secretKey := props[S3AccessKeyID], props[S3SecretAccessKey]
-		token := props[S3SessionToken]
-		if accessKey != "" || secretKey != "" || token != "" {
-			opts = append(opts, config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(
-				props[S3AccessKeyID], props[S3SecretAccessKey], props[S3SessionToken])))
-		}
-
-		if proxy, ok := props[S3ProxyURI]; ok {
-			proxyURL, err := url.Parse(proxy)
-			if err != nil {
-				return nil, fmt.Errorf("invalid s3 proxy url '%s'", proxy)
-			}
-
-			opts = append(opts, config.WithHTTPClient(awshttp.NewBuildableClient().WithTransportOptions(
-				func(t *http.Transport) {
-					t.Proxy = http.ProxyURL(proxyURL)
-				},
-			)))
-		}
-
-		awscfg, err := config.LoadDefaultConfig(context.Background(), opts...)
-		if err != nil {
-			return nil, err
-		}
-
-		preprocess := func(n string) string {
-			_, after, found := strings.Cut(n, "://")
-			if found {
-				n = after
-			}
-
-			return strings.TrimPrefix(n, parsed.Host)
-		}
-
-		s3fs := s3iofs.New(parsed.Host, awscfg)
-		return FSPreProcName(s3fs, preprocess), nil
+		return createS3FileIO(parsed, props)
 	case "file", "":
 		return LocalFS{}, nil
 	default:
