ac72a4f2c9d1e14c95b04df3fb2d1fd4881e7f76
Matt Topol <zotthewizard@gmail.com>
2024-02-13 13:02:08 -0500

    fix tests

--- catalog/rest_test.go
+++ catalog/rest_test.go
@@ -41,7 +41,7 @@ const (
 var (
 	TestHeaders = http.Header{
 		"X-Client-Version": {"0.14.1"},
-		"User-Agent":       {"GoIceberg/" + iceberg.Version()},
+		"User-Agent":       {"PyIceberg/0.5.1"},
 		"Authorization":    {"Bearer " + TestToken},
 	}
 	OAuthTestHeaders = http.Header{
