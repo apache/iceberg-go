8dcff3c49a24a87bc11a347039dac94c742ad31e
Rick Curtis <rick@deltastream.io>
2025-02-11 20:27:31 -0600

    Add fn comment and asf file header

--- utils/context.go
+++ utils/context.go
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package utils
 
 import (
@@ -8,10 +25,13 @@ import (
 
 type awsctxkey struct{}
 
+// WithAwsConfig returns a new context with the given AWS config
 func WithAwsConfig(ctx context.Context, cfg *aws.Config) context.Context {
 	return context.WithValue(ctx, awsctxkey{}, cfg)
 }
 
+// GetAwsConfig returns the AWS config from the given context. Returns
+// nil if no config is set.
 func GetAwsConfig(ctx context.Context) *aws.Config {
 	if v := ctx.Value(awsctxkey{}); v != nil {
 		return v.(*aws.Config)
