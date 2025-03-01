37d5675674d9d5008a0ca1d9a245f45d4e084948
Matt Topol <zotthewizard@gmail.com>
2025-03-01 14:15:49 -0500

    feat(table): Update Snapshot Summaries

--- table/snapshots.go
+++ table/snapshots.go
@@ -22,10 +22,12 @@ import (
 	"errors"
 	"fmt"
 	"maps"
+	"slices"
 	"strconv"
+	"strings"
 
 	"github.com/apache/iceberg-go"
-	"github.com/apache/iceberg-go/io"
+	iceio "github.com/apache/iceberg-go/io"
 )
 
 type Operation string
@@ -53,14 +55,127 @@ func ValidOperation(s string) (Operation, error) {
 	return "", fmt.Errorf("%w: found '%s'", ErrInvalidOperation, s)
 }
 
-const operationKey = "operation"
+const (
+	operationKey = "operation"
+
+	addedDataFilesKey         = "added-data-files"
+	addedDeleteFilesKey       = "added-delete-files"
+	addedEqDeletesKey         = "added-equality-deletes"
+	addedFileSizeKey          = "added-files-size"
+	addedPosDeletesKey        = "added-position-deletes"
+	addedPosDeleteFilesKey    = "added-position-delete-files"
+	addedRecordsKey           = "added-records"
+	addedEqDeleteFilesKey     = "added-equality-delete-files"
+	deletedDataFilesKey       = "deleted-data-files"
+	deletedRecordsKey         = "deleted-records"
+	removedDeleteFilesKey     = "removed-delete-files"
+	removedEqDeletesKey       = "removed-equality-deletes"
+	removedEqDeleteFilesKey   = "removed-equality-delete-files"
+	removedFileSizeKey        = "removed-files-size"
+	removedPosDeletesKey      = "removed-position-deletes"
+	removedPosDeleteFilesKey  = "removed-position-delete-files"
+	totalEqDeletesKey         = "total-equality-deletes"
+	totalPosDeletesKey        = "total-position-deletes"
+	totalDataFilesKey         = "total-data-files"
+	totalDeleteFilesKey       = "total-delete-files"
+	totalRecordsKey           = "total-records"
+	totalFileSizeKey          = "total-files-size"
+	changedPartitionCountProp = "changed-partition-count"
+	changedPartitionPrefix    = "partitions."
+)
+
+type updateMetrics struct {
+	addedFileSize         int64
+	removedFileSize       int64
+	addedDataFiles        int64
+	removedDataFiles      int64
+	addedEqDeleteFiles    int64
+	removedEqDeleteFiles  int64
+	addedPosDeleteFiles   int64
+	removedPosDeleteFiles int64
+	addedDeleteFiles      int64
+	removedDeleteFiles    int64
+	addedRecords          int64
+	deletedRecords        int64
+	addedPosDeletes       int64
+	removedPosDeletes     int64
+	addedEqDeletes        int64
+	removedEqDeletes      int64
+}
+
+func (m *updateMetrics) addFile(df iceberg.DataFile) error {
+	m.addedFileSize += df.FileSizeBytes()
+	switch df.ContentType() {
+	case iceberg.EntryContentData:
+		m.addedDataFiles++
+		m.addedRecords += df.Count()
+	case iceberg.EntryContentPosDeletes:
+		m.addedDeleteFiles++
+		m.addedPosDeleteFiles++
+		m.addedPosDeletes += df.Count()
+	case iceberg.EntryContentEqDeletes:
+		m.addedDeleteFiles++
+		m.addedEqDeleteFiles++
+		m.addedEqDeletes += df.Count()
+	default:
+		return fmt.Errorf("unknown data file content: %s", df.ContentType())
+	}
+	return nil
+}
+
+func (m *updateMetrics) removeFile(df iceberg.DataFile) error {
+	m.removedFileSize += df.FileSizeBytes()
+	switch df.ContentType() {
+	case iceberg.EntryContentData:
+		m.removedDataFiles++
+		m.deletedRecords += df.Count()
+	case iceberg.EntryContentPosDeletes:
+		m.removedDeleteFiles++
+		m.removedPosDeleteFiles++
+		m.removedPosDeletes += df.Count()
+	case iceberg.EntryContentEqDeletes:
+		m.removedDeleteFiles++
+		m.removedEqDeleteFiles++
+		m.removedEqDeletes += df.Count()
+	default:
+		return fmt.Errorf("unknown data file content: %s", df.ContentType())
+	}
+	return nil
+}
+
+func setWhenPositive(props iceberg.Properties, key string, value int64) {
+	if value > 0 {
+		props[key] = strconv.FormatInt(value, 10)
+	}
+}
+
+func (m *updateMetrics) toProps() iceberg.Properties {
+	props := iceberg.Properties{}
+	setWhenPositive(props, addedFileSizeKey, m.addedFileSize)
+	setWhenPositive(props, removedFileSizeKey, m.removedFileSize)
+	setWhenPositive(props, addedDataFilesKey, m.addedDataFiles)
+	setWhenPositive(props, deletedDataFilesKey, m.removedDataFiles)
+	setWhenPositive(props, addedEqDeleteFilesKey, m.addedEqDeleteFiles)
+	setWhenPositive(props, removedEqDeleteFilesKey, m.removedEqDeleteFiles)
+	setWhenPositive(props, addedPosDeleteFilesKey, m.addedPosDeleteFiles)
+	setWhenPositive(props, removedPosDeleteFilesKey, m.removedPosDeleteFiles)
+	setWhenPositive(props, addedDeleteFilesKey, m.addedDeleteFiles)
+	setWhenPositive(props, removedDeleteFilesKey, m.removedDeleteFiles)
+	setWhenPositive(props, addedRecordsKey, m.addedRecords)
+	setWhenPositive(props, deletedRecordsKey, m.deletedRecords)
+	setWhenPositive(props, addedPosDeletesKey, m.addedPosDeletes)
+	setWhenPositive(props, removedPosDeletesKey, m.removedPosDeletes)
+	setWhenPositive(props, addedEqDeletesKey, m.addedEqDeletes)
+	setWhenPositive(props, removedEqDeletesKey, m.removedEqDeletes)
+	return props
+}
 
 // Summary stores the summary information for a snapshot indicating
 // the operation that created the snapshot, and various properties
 // which might exist in the summary.
 type Summary struct {
 	Operation  Operation
-	Properties map[string]string
+	Properties iceberg.Properties
 }
 
 func (s *Summary) String() string {
@@ -174,7 +289,7 @@ func (s Snapshot) Equals(other Snapshot) bool {
 		s.Summary.Equals(other.Summary)
 }
 
-func (s Snapshot) Manifests(fio io.IO) ([]iceberg.ManifestFile, error) {
+func (s Snapshot) Manifests(fio iceio.IO) ([]iceberg.ManifestFile, error) {
 	if s.ManifestList != "" {
 		f, err := fio.Open(s.ManifestList)
 		if err != nil {
@@ -196,4 +311,151 @@ type MetadataLogEntry struct {
 type SnapshotLogEntry struct {
 	SnapshotID  int64 `json:"snapshot-id"`
 	TimestampMs int64 `json:"timestamp-ms"`
+}
+
+type snapshotSummaryCollector struct {
+	metrics                          updateMetrics
+	partitionMetrics                 map[string]updateMetrics
+	maxChangedPartitionsForSummaries int
+}
+
+func (s *snapshotSummaryCollector) setPartitionSummaryLimit(limit int) {
+	s.maxChangedPartitionsForSummaries = limit
+}
+
+func (s *snapshotSummaryCollector) updatePartitionMetrics(spec iceberg.PartitionSpec, df iceberg.DataFile, isAddFile bool, sc *iceberg.Schema) error {
+	partitionPath := spec.PartitionToPath(getPartitionRecord(df, spec.PartitionType(sc)), sc)
+	if s.partitionMetrics == nil {
+		s.partitionMetrics = make(map[string]updateMetrics)
+	}
+
+	metrics := s.partitionMetrics[partitionPath]
+	if isAddFile {
+		if err := metrics.addFile(df); err != nil {
+			return err
+		}
+	} else {
+		if err := metrics.removeFile(df); err != nil {
+			return err
+		}
+	}
+	s.partitionMetrics[partitionPath] = metrics
+
+	return nil
+}
+
+func (s *snapshotSummaryCollector) addFile(df iceberg.DataFile, sc *iceberg.Schema, spec iceberg.PartitionSpec) error {
+	if err := s.metrics.addFile(df); err != nil {
+		return err
+	}
+
+	if len(df.Partition()) > 0 {
+		return s.updatePartitionMetrics(spec, df, true, sc)
+	}
+	return nil
+}
+
+func (s *snapshotSummaryCollector) removeFile(df iceberg.DataFile, sc *iceberg.Schema, spec iceberg.PartitionSpec) error {
+	if err := s.metrics.removeFile(df); err != nil {
+		return err
+	}
+
+	if len(df.Partition()) > 0 {
+		return s.updatePartitionMetrics(spec, df, false, sc)
+	}
+	return nil
+}
+
+func (s *snapshotSummaryCollector) partitionSummary(metrics *updateMetrics) string {
+	props := metrics.toProps()
+	return strings.Join(slices.Collect(func(yield func(s string) bool) {
+		for k, v := range props {
+			if !yield(fmt.Sprintf("%s=%s", k, v)) {
+				return
+			}
+		}
+	}), ",")
+}
+
+func (s *snapshotSummaryCollector) build() iceberg.Properties {
+	props := s.metrics.toProps()
+	changedPartitionsSize := len(s.partitionMetrics)
+	setWhenPositive(props, changedPartitionCountProp, int64(changedPartitionsSize))
+	if changedPartitionsSize <= s.maxChangedPartitionsForSummaries {
+		for partPath, updateMetricsPart := range s.partitionMetrics {
+			if summary := s.partitionSummary(&updateMetricsPart); len(summary) > 0 {
+				props[changedPartitionPrefix+partPath] = summary
+			}
+		}
+	}
+	return props
+}
+
+func truncateTableSummary(sum Summary, previous iceberg.Properties) Summary {
+	keys := []string{totalDataFilesKey, totalDeleteFilesKey, totalRecordsKey,
+		totalFileSizeKey, totalPosDeletesKey, totalEqDeletesKey}
+	for _, prop := range keys {
+		sum.Properties[prop] = "0"
+	}
+
+	updateProp := func(prop, prevProp string) {
+		if val := previous.GetInt(prevProp, 0); val > 0 {
+			sum.Properties[prop] = strconv.Itoa(val)
+		}
+	}
+
+	updateProp(deletedDataFilesKey, totalDataFilesKey)
+	updateProp(removedDeleteFilesKey, totalDeleteFilesKey)
+	updateProp(deletedRecordsKey, totalRecordsKey)
+	updateProp(removedFileSizeKey, totalFileSizeKey)
+	updateProp(removedPosDeletesKey, totalPosDeletesKey)
+	updateProp(removedEqDeletesKey, totalEqDeletesKey)
+
+	return sum
+}
+
+func updateSnapshotSummaries(sum Summary, previous iceberg.Properties, truncateFullTable bool) (Summary, error) {
+	switch sum.Operation {
+	case OpAppend, OpOverwrite, OpDelete:
+	default:
+		return sum, fmt.Errorf("%w: operation: %s", iceberg.ErrNotImplemented, sum.Operation)
+	}
+
+	if sum.Properties == nil {
+		sum.Properties = make(iceberg.Properties)
+	}
+
+	if truncateFullTable && sum.Operation == OpOverwrite && previous != nil {
+		sum = truncateTableSummary(sum, previous)
+	}
+
+	if previous == nil {
+		previous = iceberg.Properties{
+			totalDataFilesKey:   "0",
+			totalDeleteFilesKey: "0",
+			totalRecordsKey:     "0",
+			totalFileSizeKey:    "0",
+			totalPosDeletesKey:  "0",
+			totalEqDeletesKey:   "0",
+		}
+	}
+
+	updateTotals := func(totalProp, addedProp, removedProp string) {
+		newTotal := previous.GetInt(totalProp, 0)
+		newTotal += sum.Properties.GetInt(addedProp, 0)
+		newTotal -= sum.Properties.GetInt(removedProp, 0)
+
+		if newTotal >= 0 {
+			sum.Properties[totalProp] = strconv.Itoa(newTotal)
+		}
+	}
+
+	updateTotals(totalDataFilesKey, addedDataFilesKey, deletedDataFilesKey)
+	updateTotals(totalDeleteFilesKey, addedDeleteFilesKey, removedDeleteFilesKey)
+	updateTotals(totalRecordsKey, addedRecordsKey, deletedRecordsKey)
+	updateTotals(totalFileSizeKey, addedFileSizeKey, removedFileSizeKey)
+	updateTotals(totalPosDeletesKey, addedPosDeletesKey, removedPosDeletesKey)
+	updateTotals(totalEqDeletesKey, addedEqDeletesKey, removedEqDeletesKey)
+
+	return sum, nil
 }
---
+++ table/snapshots_internal_test.go
@@ -0,0 +1,191 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package table
+
+import (
+	"testing"
+
+	"github.com/apache/iceberg-go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	tableSchemaSimple = iceberg.NewSchemaWithIdentifiers(1,
+		[]int{2},
+		iceberg.NestedField{ID: 1, Name: "foo", Type: iceberg.PrimitiveTypes.String},
+		iceberg.NestedField{ID: 2, Name: "bar", Type: iceberg.PrimitiveTypes.Int32, Required: true},
+		iceberg.NestedField{ID: 3, Name: "baz", Type: iceberg.PrimitiveTypes.Bool},
+	)
+)
+
+func TestSnapshotSummaryCollector(t *testing.T) {
+	var ssc snapshotSummaryCollector
+
+	assert.Equal(t, iceberg.Properties{}, ssc.build())
+
+	dataFile, err := iceberg.NewDataFileBuilder(*iceberg.UnpartitionedSpec,
+		iceberg.EntryContentData, "/path/to/file.parquet", iceberg.ParquetFile, nil, 100, 1234)
+	require.NoError(t, err)
+	require.NoError(t, ssc.addFile(dataFile.Build(), tableSchemaSimple, *iceberg.UnpartitionedSpec))
+
+	assert.Equal(t, iceberg.Properties{
+		"added-data-files": "1",
+		"added-files-size": "1234",
+		"added-records":    "100",
+	}, ssc.build())
+}
+
+func TestSnapshotSummaryCollectorWithPartition(t *testing.T) {
+	var ssc snapshotSummaryCollector
+
+	assert.Equal(t, iceberg.Properties{}, ssc.build())
+	sc := iceberg.NewSchema(0,
+		iceberg.NestedField{ID: 1, Name: "bool_field", Type: iceberg.PrimitiveTypes.Bool},
+		iceberg.NestedField{ID: 2, Name: "string_field", Type: iceberg.PrimitiveTypes.String},
+		iceberg.NestedField{ID: 3, Name: "int_field", Type: iceberg.PrimitiveTypes.Int32},
+	)
+	spec := iceberg.NewPartitionSpec(iceberg.PartitionField{
+		Name: "int_field", SourceID: 3, FieldID: 1001, Transform: iceberg.IdentityTransform{},
+	})
+
+	dataFile1 := must(iceberg.NewDataFileBuilder(
+		spec, iceberg.EntryContentData, "/path/to/file1.parquet",
+		iceberg.ParquetFile, map[string]any{"int_field": int32(1)}, 100, 1234)).Build()
+
+	dataFile2 := must(iceberg.NewDataFileBuilder(
+		spec, iceberg.EntryContentData, "/path/to/file2.parquet",
+		iceberg.ParquetFile, map[string]any{"int_field": int32(2)}, 200, 4321)).Build()
+
+	ssc.addFile(dataFile1, sc, spec)
+	ssc.removeFile(dataFile1, sc, spec)
+	ssc.removeFile(dataFile2, sc, spec)
+
+	assert.Equal(t, iceberg.Properties{
+		"added-files-size":        "1234",
+		"removed-files-size":      "5555",
+		"added-data-files":        "1",
+		"deleted-data-files":      "2",
+		"added-records":           "100",
+		"deleted-records":         "300",
+		"changed-partition-count": "2",
+	}, ssc.build())
+
+	ssc.setPartitionSummaryLimit(10)
+	assert.Equal(t, iceberg.Properties{
+		"added-files-size":        "1234",
+		"removed-files-size":      "5555",
+		"added-data-files":        "1",
+		"deleted-data-files":      "2",
+		"added-records":           "100",
+		"deleted-records":         "300",
+		"changed-partition-count": "2",
+		"partitions.int_field=1":  "added-files-size=1234,removed-files-size=1234,added-data-files=1,deleted-data-files=1,added-records=100,deleted-records=100",
+		"partitions.int_field=2":  "removed-files-size=4321,deleted-data-files=1,deleted-records=200",
+	}, ssc.build())
+}
+
+func TestMergeSnapshotSummaries(t *testing.T) {
+	tests := []struct {
+		sum           Summary
+		previous      iceberg.Properties
+		truncateTable bool
+		expected      Summary
+	}{
+		{Summary{Operation: OpAppend}, nil, false,
+			Summary{Operation: OpAppend, Properties: iceberg.Properties{
+				"total-data-files":       "0",
+				"total-delete-files":     "0",
+				"total-records":          "0",
+				"total-files-size":       "0",
+				"total-position-deletes": "0",
+				"total-equality-deletes": "0",
+			}}},
+		{Summary{Operation: OpAppend, Properties: iceberg.Properties{
+			"added-data-files":       "1",
+			"added-delete-files":     "2",
+			"added-equality-deletes": "3",
+			"added-files-size":       "4",
+			"added-position-deletes": "5",
+			"added-records":          "6",
+		}}, nil, false,
+			Summary{Operation: OpAppend, Properties: iceberg.Properties{
+				"added-data-files":       "1",
+				"added-delete-files":     "2",
+				"added-equality-deletes": "3",
+				"added-files-size":       "4",
+				"added-position-deletes": "5",
+				"added-records":          "6",
+				"total-data-files":       "1",
+				"total-delete-files":     "2",
+				"total-records":          "6",
+				"total-files-size":       "4",
+				"total-position-deletes": "5",
+				"total-equality-deletes": "3",
+			}}},
+		{
+			Summary{Operation: OpOverwrite, Properties: iceberg.Properties{
+				"added-data-files":       "1",
+				"added-delete-files":     "2",
+				"added-equality-deletes": "3",
+				"added-files-size":       "4",
+				"added-position-deletes": "5",
+				"added-records":          "6",
+			}},
+			iceberg.Properties{
+				"total-data-files":       "1",
+				"total-delete-files":     "1",
+				"total-equality-deletes": "1",
+				"total-files-size":       "1",
+				"total-position-deletes": "1",
+				"total-records":          "1",
+			},
+			true, Summary{Operation: OpOverwrite, Properties: iceberg.Properties{
+				"added-data-files":         "1",
+				"added-delete-files":       "2",
+				"added-equality-deletes":   "3",
+				"added-files-size":         "4",
+				"added-position-deletes":   "5",
+				"added-records":            "6",
+				"total-data-files":         "1",
+				"total-records":            "6",
+				"total-delete-files":       "2",
+				"total-equality-deletes":   "3",
+				"total-files-size":         "4",
+				"total-position-deletes":   "5",
+				"deleted-data-files":       "1",
+				"removed-delete-files":     "1",
+				"deleted-records":          "1",
+				"removed-files-size":       "1",
+				"removed-position-deletes": "1",
+				"removed-equality-deletes": "1",
+			}},
+		},
+	}
+
+	for _, tt := range tests {
+		result, err := updateSnapshotSummaries(tt.sum, tt.previous, tt.truncateTable)
+		require.NoError(t, err)
+		assert.Equal(t, tt.expected, result)
+	}
+}
+
+func TestInvalidOperation(t *testing.T) {
+	_, err := updateSnapshotSummaries(Summary{Operation: OpReplace}, nil, false)
+	assert.ErrorIs(t, err, iceberg.ErrNotImplemented)
+}
