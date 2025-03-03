b8c0e275b854161b0e6cd6bf9d4338dcecb2e2c3
Matt Topol <zotthewizard@gmail.com>
2025-03-03 13:00:17 -0500

    feat(internal): Adding BinPacker

---
+++ internal/utils.go
@@ -0,0 +1,112 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package internal
+
+import (
+	"cmp"
+	"slices"
+)
+
+type Bin[T any] struct {
+	binWeight    int64
+	targetWeight int64
+	items        []T
+}
+
+func (b *Bin[T]) Weight() int64            { return b.binWeight }
+func (b *Bin[T]) CanAdd(weight int64) bool { return b.binWeight+weight <= b.targetWeight }
+func (b *Bin[T]) Add(item T, weight int64) {
+	b.binWeight += weight
+	b.items = append(b.items, item)
+}
+
+type SlicePacker[T any] struct {
+	TargetWeight    int64
+	Lookback        int
+	LargestBinFirst bool
+}
+
+func (s *SlicePacker[T]) Pack(items []T, weightFunc func(T) int64) [][]T {
+	bins := make([]Bin[T], 0)
+	findBin := func(weight int64) *Bin[T] {
+		for i := range bins {
+			if bins[i].CanAdd(weight) {
+				return &bins[i]
+			}
+		}
+		return nil
+	}
+
+	removeBin := func() Bin[T] {
+		if s.LargestBinFirst {
+			maxBin := slices.MaxFunc(bins, func(a, b Bin[T]) int {
+				return cmp.Compare(a.Weight(), b.Weight())
+			})
+			i := slices.IndexFunc(bins, func(e Bin[T]) bool {
+				return e.Weight() == maxBin.Weight()
+			})
+
+			bins = slices.Delete(bins, i, i+1)
+			return maxBin
+		}
+
+		var out Bin[T]
+		out, bins = bins[0], bins[1:]
+		return out
+	}
+
+	return slices.Collect(func(yield func([]T) bool) {
+		for _, item := range items {
+			w := weightFunc(item)
+			bin := findBin(w)
+			if bin != nil {
+				bin.Add(item, w)
+			} else {
+				bin := Bin[T]{targetWeight: s.TargetWeight}
+				bin.Add(item, w)
+				bins = append(bins, bin)
+
+				if len(bins) > s.Lookback {
+					if !yield(removeBin().items) {
+						return
+					}
+				}
+			}
+		}
+
+		for len(bins) > 0 {
+			if !yield(removeBin().items) {
+				return
+			}
+		}
+	})
+
+}
+
+func (s *SlicePacker[T]) PackEnd(items []T, weightFunc func(T) int64) [][]T {
+	slices.Reverse(items)
+	packed := s.Pack(items, weightFunc)
+	slices.Reverse(packed)
+
+	result := make([][]T, 0, len(packed))
+	for _, items := range packed {
+		slices.Reverse(items)
+		result = append(result, items)
+	}
+	return result
+}
---
+++ internal/utils_test.go
@@ -0,0 +1,118 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package internal_test
+
+import (
+	"math"
+	"testing"
+
+	"github.com/apache/iceberg-go/internal"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBinPacking(t *testing.T) {
+	tests := []struct {
+		splits          []int
+		tgtWeight       int64
+		lookback        int
+		largestBinFirst bool
+		expected        [][]int
+	}{
+		{[]int{36, 36, 36, 36, 73, 110, 128}, 128, 2, true,
+			[][]int{{110}, {128}, {36, 73}, {36, 36, 36}}},
+		{[]int{36, 36, 36, 36, 73, 110, 128}, 128, 2, false,
+			[][]int{{36, 36, 36}, {36, 73}, {110}, {128}}},
+		{[]int{64, 64, 128, 32, 32, 32, 32}, 128, 1, true,
+			[][]int{{64, 64}, {128}, {32, 32, 32, 32}}},
+		{[]int{64, 64, 128, 32, 32, 32, 32}, 128, 1, false,
+			[][]int{{64, 64}, {128}, {32, 32, 32, 32}}},
+	}
+
+	weightFn := func(i int) int64 {
+		return int64(i)
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			p := &internal.SlicePacker[int]{
+				TargetWeight:    tt.tgtWeight,
+				Lookback:        tt.lookback,
+				LargestBinFirst: tt.largestBinFirst,
+			}
+
+			bins := p.Pack(tt.splits, weightFn)
+			assert.Equal(t, tt.expected, bins)
+		})
+	}
+}
+
+func TestReverseBinPackingLookback(t *testing.T) {
+	intmax := math.MaxInt32
+
+	tests := []struct {
+		splits          []int
+		tgtWeight       int64
+		lookback        int
+		largestBinFirst bool
+		expected        [][]int
+	}{
+		// single lookback tests
+		{[]int{1, 2, 3, 4, 5}, 3, 1, false, [][]int{{1, 2}, {3}, {4}, {5}}},
+		{[]int{1, 2, 3, 4, 5}, 4, 1, false, [][]int{{1, 2}, {3}, {4}, {5}}},
+		{[]int{1, 2, 3, 4, 5}, 5, 1, false, [][]int{{1}, {2, 3}, {4}, {5}}},
+		{[]int{1, 2, 3, 4, 5}, 6, 1, false, [][]int{{1, 2, 3}, {4}, {5}}},
+		{[]int{1, 2, 3, 4, 5}, 7, 1, false, [][]int{{1, 2}, {3, 4}, {5}}},
+		{[]int{1, 2, 3, 4, 5}, 8, 1, false, [][]int{{1, 2}, {3, 4}, {5}}},
+		{[]int{1, 2, 3, 4, 5}, 9, 1, false, [][]int{{1, 2, 3}, {4, 5}}},
+		{[]int{1, 2, 3, 4, 5}, 11, 1, false, [][]int{{1, 2, 3}, {4, 5}}},
+		{[]int{1, 2, 3, 4, 5}, 12, 1, false, [][]int{{1, 2}, {3, 4, 5}}},
+		{[]int{1, 2, 3, 4, 5}, 14, 1, false, [][]int{{1}, {2, 3, 4, 5}}},
+		{[]int{1, 2, 3, 4, 5}, 15, 1, false, [][]int{{1, 2, 3, 4, 5}}},
+		// unlimited lookback tests
+		{[]int{1, 2, 3, 4, 5}, 3, intmax, false, [][]int{{1, 2}, {3}, {4}, {5}}},
+		{[]int{1, 2, 3, 4, 5}, 4, intmax, false, [][]int{{2}, {1, 3}, {4}, {5}}},
+		{[]int{1, 2, 3, 4, 5}, 5, intmax, false, [][]int{{2, 3}, {1, 4}, {5}}},
+		{[]int{1, 2, 3, 4, 5}, 6, intmax, false, [][]int{{3}, {2, 4}, {1, 5}}},
+		{[]int{1, 2, 3, 4, 5}, 7, intmax, false, [][]int{{1}, {3, 4}, {2, 5}}},
+		{[]int{1, 2, 3, 4, 5}, 8, intmax, false, [][]int{{1, 2, 4}, {3, 5}}},
+		{[]int{1, 2, 3, 4, 5}, 9, intmax, false, [][]int{{1, 2, 3}, {4, 5}}},
+		{[]int{1, 2, 3, 4, 5}, 10, intmax, false, [][]int{{2, 3}, {1, 4, 5}}},
+		{[]int{1, 2, 3, 4, 5}, 11, intmax, false, [][]int{{1, 3}, {2, 4, 5}}},
+		{[]int{1, 2, 3, 4, 5}, 12, intmax, false, [][]int{{1, 2}, {3, 4, 5}}},
+		{[]int{1, 2, 3, 4, 5}, 13, intmax, false, [][]int{{2}, {1, 3, 4, 5}}},
+		{[]int{1, 2, 3, 4, 5}, 14, intmax, false, [][]int{{1}, {2, 3, 4, 5}}},
+		{[]int{1, 2, 3, 4, 5}, 15, intmax, false, [][]int{{1, 2, 3, 4, 5}}},
+	}
+
+	weightFn := func(i int) int64 {
+		return int64(i)
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			p := &internal.SlicePacker[int]{
+				TargetWeight:    tt.tgtWeight,
+				Lookback:        tt.lookback,
+				LargestBinFirst: tt.largestBinFirst,
+			}
+
+			bins := p.PackEnd(tt.splits, weightFn)
+			assert.Equal(t, tt.expected, bins)
+		})
+	}
+}
