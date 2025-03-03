eda642b6022f628ca95fcedd230dce8083ebb7bb
Matt Topol <zotthewizard@gmail.com>
2025-03-03 12:54:59 -0500

    feat(table): add location provider

--- table/table.go
+++ table/table.go
@@ -40,11 +40,10 @@ func (t Table) Equals(other Table) bool {
 		t.metadata.Equals(other.metadata)
 }
 
-func (t Table) Identifier() Identifier   { return t.identifier }
-func (t Table) Metadata() Metadata       { return t.metadata }
-func (t Table) MetadataLocation() string { return t.metadataLocation }
-func (t Table) FS() io.IO                { return t.fs }
-
+func (t Table) Identifier() Identifier               { return t.identifier }
+func (t Table) Metadata() Metadata                   { return t.metadata }
+func (t Table) MetadataLocation() string             { return t.metadataLocation }
+func (t Table) FS() io.IO                            { return t.fs }
 func (t Table) Schema() *iceberg.Schema              { return t.metadata.CurrentSchema() }
 func (t Table) Spec() iceberg.PartitionSpec          { return t.metadata.PartitionSpec() }
 func (t Table) SortOrder() SortOrder                 { return t.metadata.SortOrder() }
@@ -60,6 +59,10 @@ func (t Table) Schemas() map[int]*iceberg.Schema {
 	}
 
 	return m
+}
+
+func (t Table) LocationProvider() (LocationProvider, error) {
+	return LoadLocationProvider(t.metadataLocation, t.metadata.Properties())
 }
 
 type ScanOption func(*Scan)
