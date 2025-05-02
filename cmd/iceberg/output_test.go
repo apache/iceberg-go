67a26d1ec4d3080db894def3b36359759c17dee0
Oleksandr Khomin <37224169+okhomin@users.noreply.github.com>
2025-05-02 10:17:59 -0700

    feat(cmd/iceberg): Implement json output format (#413)
    implements #325 
    
    ### Summary:
    Implement `json` output format for the CLI tool.
    
    ### Changes:
    * Implement `jsonOutput` type
    * Rename `text` to `textOutput`
    
    ### Example:
    ```sh
    $ go run ./cmd/iceberg --uri http://127.0.0.1:8181 --output json describe table 'nyc.taxis'
    {"metadata":{"last-sequence-number":0,"format-version":2,"table-uuid":"643a61a3-32a3-4649-b6ce-e2aee49d8896","location":"s3://warehouse/nyc/taxis","last-updated-ms":1746128668331,"last-column-id":5,"schemas":[{"type":"struct","fields":[{"type":"long","id":1,"name":"vendor_id","required":false},{"type":"long","id":2,"name":"trip_id","required":false},{"type":"float","id":3,"name":"trip_distance","required":false},{"type":"double","id":4,"name":"fare_amount","required":false},{"type":"string","id":5,"name":"store_and_fwd_flag","required":false}],"schema-id":0,"identifier-field-ids":[]}],"current-schema-id":0,"partition-specs":[{"spec-id":0,"fields":[{"source-id":1,"field-id":1000,"name":"vendor_id","transform":"identity"}]}],"default-spec-id":0,"last-partition-id":1000,"properties":{"owner":"root","write.parquet.compression-codec":"zstd"},"sort-orders":[{"order-id":0,"fields":[]}],"default-sort-order-id":0},"metadata-location":"s3://warehouse/nyc/taxis/metadata/00000-e040619a-e417-4096-bbd8-d29333cbd6f9.metadata.json","sort-order":{"order-id":0,"fields":[]},"spec":{"spec-id":0,"fields":[{"source-id":1,"field-id":1000,"name":"vendor_id","transform":"identity"}]},"schema":{"type":"struct","fields":[{"type":"long","id":1,"name":"vendor_id","required":false},{"type":"long","id":2,"name":"trip_id","required":false},{"type":"float","id":3,"name":"trip_distance","required":false},{"type":"double","id":4,"name":"fare_amount","required":false},{"type":"string","id":5,"name":"store_and_fwd_flag","required":false}],"schema-id":0,"identifier-field-ids":[]}}
    ```
    
    ---------
    
    Co-authored-by: Oleksandr Khomin <okhomi@amazon.com>

--- cmd/iceberg/output_test.go
+++ cmd/iceberg/output_test.go
@@ -19,6 +19,7 @@ package main
 
 import (
 	"bytes"
+	"os"
 	"testing"
 
 	"github.com/apache/iceberg-go/table"
@@ -26,68 +27,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var testArgs = []struct {
-	meta     string
-	expected string
-}{
-	{
-		`{
-    "format-version": 2,
-    "table-uuid": "9c12d441-03fe-4693-9a96-a0705ddf69c1",
-    "location": "s3://bucket/test/location",
-    "last-sequence-number": 0,
-    "last-updated-ms": 1602638573590,
-    "last-column-id": 3,
-    "current-schema-id": 0,
-    "schemas": [
-        {"type": "struct", "schema-id": 0, "fields": [{"id": 1, "name": "x", "required": true, "type": "long"}]},
-        {
-            "type": "struct",
-            "fields": [
-                {"id": 1, "name": "x", "required": true, "type": "long"}
-            ]
-        }
-    ],
-    "default-spec-id": 0,
-    "partition-specs": [{"spec-id": 0, "fields": []}],
-    "last-partition-id": 1000,
-    "default-sort-order-id": 0,
-    "sort-orders": [
-        {
-            "order-id": 0,
-            "fields": [ ]
-        }
-    ],
-    "properties": {"read.split.target.size": "134217728"},
-    "current-snapshot-id": -1,
-    "snapshots": [ ],
-    "snapshot-log": [ ],
-    "metadata-log": [ ],
-    "refs": { }
-}`,
-		`Table format version | 2                                   
-Metadata location    |                                     
-Table UUID           | 9c12d441-03fe-4693-9a96-a0705ddf69c1
-Last updated         | 1602638573590                       
-Sort Order           | 0: []                               
-Partition Spec       | []                                  
-
-Current Schema, id=0
-└──1: x: required long
-
-Current Snapshot | 
-
-Snapshots
-
-Properties
-key                    | value    
-----------------------------------
-read.split.target.size | 134217728
-
-`,
-	},
-	{
-		`{
+func Test_textOutput_DescribeTable(t *testing.T) {
+	type args struct {
+		meta string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected string
+	}{
+		{
+			name: "Describe a table",
+			args: args{
+				meta: `{
     "format-version": 2,
     "table-uuid": "9c12d441-03fe-4693-9a96-a0705ddf69c1",
     "location": "s3://bucket/test/location",
@@ -149,7 +101,8 @@ read.split.target.size | 134217728
     "metadata-log": [{"metadata-file": "s3://bucket/.../v1.json", "timestamp-ms": 1515100}],
     "refs": {"test": {"snapshot-id": 3051729675574597004, "type": "tag", "max-ref-age-ms": 10000000}}
 }`,
-		`Table format version | 2                                   
+			},
+			expected: `Table format version | 2                                   
 Metadata location    |                                     
 Table UUID           | 9c12d441-03fe-4693-9a96-a0705ddf69c1
 Last updated         | 1602638573590                       
@@ -178,21 +131,279 @@ key                    | value
 read.split.target.size | 134217728
 
 `,
-	},
+		},
+		{
+			name: "Describe a table with empty objects",
+			args: args{
+				meta: `{
+    "format-version": 2,
+    "table-uuid": "9c12d441-03fe-4693-9a96-a0705ddf69c1",
+    "location": "s3://bucket/test/location",
+    "last-sequence-number": 0,
+    "last-updated-ms": 1602638573590,
+    "last-column-id": 3,
+    "current-schema-id": 0,
+    "schemas": [
+        {"type": "struct", "schema-id": 0, "fields": [{"id": 1, "name": "x", "required": true, "type": "long"}]},
+        {
+            "type": "struct",
+            "fields": [
+                {"id": 1, "name": "x", "required": true, "type": "long"}
+            ]
+        }
+    ],
+    "default-spec-id": 0,
+    "partition-specs": [{"spec-id": 0, "fields": []}],
+    "last-partition-id": 1000,
+    "default-sort-order-id": 0,
+    "sort-orders": [
+        {
+            "order-id": 0,
+            "fields": [ ]
+        }
+    ],
+    "properties": {"read.split.target.size": "134217728"},
+    "current-snapshot-id": -1,
+    "snapshots": [ ],
+    "snapshot-log": [ ],
+    "metadata-log": [ ],
+    "refs": { }
+}`,
+			},
+			expected: `Table format version | 2                                   
+Metadata location    |                                     
+Table UUID           | 9c12d441-03fe-4693-9a96-a0705ddf69c1
+Last updated         | 1602638573590                       
+Sort Order           | 0: []                               
+Partition Spec       | []                                  
+
+Current Schema, id=0
+└──1: x: required long
+
+Current Snapshot | 
+
+Snapshots
+
+Properties
+key                    | value    
+----------------------------------
+read.split.target.size | 134217728
+
+`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			pterm.SetDefaultOutput(&buf)
+			pterm.DisableColor()
+
+			meta, _ := table.ParseMetadataBytes([]byte(tt.args.meta))
+			tbl := table.New([]string{"t"}, meta, "", nil, nil)
+			buf.Reset()
+
+			textOutput{}.DescribeTable(tbl)
+
+			assert.Equal(t, tt.expected, buf.String())
+		})
+	}
 }
 
-func TestDescribeTable(t *testing.T) {
-	var buf bytes.Buffer
-	pterm.SetDefaultOutput(&buf)
-	pterm.DisableColor()
+func Test_jsonOutput_DescribeTable(t *testing.T) {
+	type args struct {
+		meta string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected string
+	}{
+		{
+			name: "Describe a table",
+			args: args{
+				meta: `{
+    "format-version": 2,
+    "table-uuid": "9c12d441-03fe-4693-9a96-a0705ddf69c1",
+    "location": "s3://bucket/test/location",
+    "last-sequence-number": 34,
+    "last-updated-ms": 1602638573590,
+    "last-column-id": 3,
+    "current-schema-id": 1,
+    "schemas": [{
+            "type": "struct",
+            "schema-id": 0,
+            "fields": [{
+                "id": 1,
+                "name": "x",
+                "required": true,
+                "type": "long"
+            }]
+        },
+        {
+            "type": "struct",
+            "schema-id": 1,
+            "identifier-field-ids": [1, 2],
+            "fields": [{
+                    "id": 1,
+                    "name": "x",
+                    "required": true,
+                    "type": "long"
+                },
+                {
+                    "id": 2,
+                    "name": "y",
+                    "required": true,
+                    "type": "long",
+                    "doc": "comment"
+                },
+                {
+                    "id": 3,
+                    "name": "z",
+                    "required": true,
+                    "type": "long"
+                }
+            ]
+        }
+    ],
+    "default-spec-id": 0,
+    "partition-specs": [{
+        "spec-id": 0,
+        "fields": [{
+            "name": "x",
+            "transform": "identity",
+            "source-id": 1,
+            "field-id": 1000
+        }]
+    }],
+    "last-partition-id": 1000,
+    "default-sort-order-id": 3,
+    "sort-orders": [{
+        "order-id": 3,
+        "fields": [{
+                "transform": "identity",
+                "source-id": 2,
+                "direction": "asc",
+                "null-order": "nulls-first"
+            },
+            {
+                "transform": "bucket[4]",
+                "source-id": 3,
+                "direction": "desc",
+                "null-order": "nulls-last"
+            }
+        ]
+    }],
+    "properties": {
+        "read.split.target.size": "134217728"
+    },
+    "current-snapshot-id": 3055729675574597004,
+    "snapshots": [{
+            "snapshot-id": 3051729675574597004,
+            "timestamp-ms": 1515100955770,
+            "sequence-number": 0,
+            "summary": {
+                "operation": "append"
+            },
+            "manifest-list": "s3://a/b/1.avro",
+            "schema-id": 1
+        },
+        {
+            "snapshot-id": 3055729675574597004,
+            "parent-snapshot-id": 3051729675574597004,
+            "timestamp-ms": 1555100955770,
+            "sequence-number": 1,
+            "summary": {
+                "operation": "append"
+            },
+            "manifest-list": "s3://a/b/2.avro",
+            "schema-id": 1
+        }
+    ],
+    "snapshot-log": [{
+            "snapshot-id": 3051729675574597004,
+            "timestamp-ms": 1515100955770
+        },
+        {
+            "snapshot-id": 3055729675574597004,
+            "timestamp-ms": 1555100955770
+        }
+    ],
+    "metadata-log": [{
+        "metadata-file": "s3://bucket/.../v1.json",
+        "timestamp-ms": 1515100
+    }],
+    "refs": {
+        "test": {
+            "snapshot-id": 3051729675574597004,
+            "type": "tag",
+            "max-ref-age-ms": 10000000
+        }
+    }
+}`,
+			},
+			expected: `{"metadata":{"last-sequence-number":34,"format-version":2,"table-uuid":"9c12d441-03fe-4693-9a96-a0705ddf69c1","location":"s3://bucket/test/location","last-updated-ms":1602638573590,"last-column-id":3,"schemas":[{"type":"struct","fields":[{"type":"long","id":1,"name":"x","required":true}],"schema-id":0,"identifier-field-ids":[]},{"type":"struct","fields":[{"type":"long","id":1,"name":"x","required":true},{"type":"long","id":2,"name":"y","required":true,"doc":"comment"},{"type":"long","id":3,"name":"z","required":true}],"schema-id":1,"identifier-field-ids":[1,2]}],"current-schema-id":1,"partition-specs":[{"spec-id":0,"fields":[{"source-id":1,"field-id":1000,"name":"x","transform":"identity"}]}],"default-spec-id":0,"last-partition-id":1000,"properties":{"read.split.target.size":"134217728"},"snapshots":[{"snapshot-id":3051729675574597004,"sequence-number":0,"timestamp-ms":1515100955770,"manifest-list":"s3://a/b/1.avro","summary":{"operation":"append"},"schema-id":1},{"snapshot-id":3055729675574597004,"parent-snapshot-id":3051729675574597004,"sequence-number":1,"timestamp-ms":1555100955770,"manifest-list":"s3://a/b/2.avro","summary":{"operation":"append"},"schema-id":1}],"current-snapshot-id":3055729675574597004,"snapshot-log":[{"snapshot-id":3051729675574597004,"timestamp-ms":1515100955770},{"snapshot-id":3055729675574597004,"timestamp-ms":1555100955770}],"metadata-log":[{"metadata-file":"s3://bucket/.../v1.json","timestamp-ms":1515100}],"sort-orders":[{"order-id":3,"fields":[{"source-id":2,"transform":"identity","direction":"asc","null-order":"nulls-first"},{"source-id":3,"transform":"bucket[4]","direction":"desc","null-order":"nulls-last"}]}],"default-sort-order-id":3,"refs":{"main":{"snapshot-id":3055729675574597004,"type":"branch"},"test":{"snapshot-id":3051729675574597004,"type":"tag","max-ref-age-ms":10000000}}},"sort-order":{"order-id":3,"fields":[{"source-id":2,"transform":"identity","direction":"asc","null-order":"nulls-first"},{"source-id":3,"transform":"bucket[4]","direction":"desc","null-order":"nulls-last"}]},"current-snapshot":{"snapshot-id":3055729675574597004,"parent-snapshot-id":3051729675574597004,"sequence-number":1,"timestamp-ms":1555100955770,"manifest-list":"s3://a/b/2.avro","summary":{"operation":"append"},"schema-id":1},"spec":{"spec-id":0,"fields":[{"source-id":1,"field-id":1000,"name":"x","transform":"identity"}]},"schema":{"type":"struct","fields":[{"type":"long","id":1,"name":"x","required":true},{"type":"long","id":2,"name":"y","required":true,"doc":"comment"},{"type":"long","id":3,"name":"z","required":true}],"schema-id":1,"identifier-field-ids":[1,2]}}`,
+		},
+		{
+			name: "Describe a table with empty objects",
+			args: args{
+				meta: `{
+    "format-version": 2,
+    "table-uuid": "9c12d441-03fe-4693-9a96-a0705ddf69c1",
+    "location": "s3://bucket/test/location",
+    "last-sequence-number": 0,
+    "last-updated-ms": 1602638573590,
+    "last-column-id": 3,
+    "current-schema-id": 0,
+    "schemas": [
+        {"type": "struct", "schema-id": 0, "fields": [{"id": 1, "name": "x", "required": true, "type": "long"}]},
+        {
+            "type": "struct",
+            "fields": [
+                {"id": 1, "name": "x", "required": true, "type": "long"}
+            ]
+        }
+    ],
+    "default-spec-id": 0,
+    "partition-specs": [{"spec-id": 0, "fields": []}],
+    "last-partition-id": 1000,
+    "default-sort-order-id": 0,
+    "sort-orders": [
+        {
+            "order-id": 0,
+            "fields": [ ]
+        }
+    ],
+    "properties": {"read.split.target.size": "134217728"},
+    "current-snapshot-id": -1,
+    "snapshots": [ ],
+    "snapshot-log": [ ],
+    "metadata-log": [ ],
+    "refs": { }
+}`,
+			},
+			expected: `{"metadata":{"last-sequence-number":0,"format-version":2,"table-uuid":"9c12d441-03fe-4693-9a96-a0705ddf69c1","location":"s3://bucket/test/location","last-updated-ms":1602638573590,"last-column-id":3,"schemas":[{"type":"struct","fields":[{"type":"long","id":1,"name":"x","required":true}],"schema-id":0,"identifier-field-ids":[]},{"type":"struct","fields":[{"type":"long","id":1,"name":"x","required":true}],"schema-id":0,"identifier-field-ids":[]}],"current-schema-id":0,"partition-specs":[{"spec-id":0,"fields":[]}],"default-spec-id":0,"last-partition-id":1000,"properties":{"read.split.target.size":"134217728"},"sort-orders":[{"order-id":0,"fields":[]}],"default-sort-order-id":0},"sort-order":{"order-id":0,"fields":[]},"spec":{"spec-id":0,"fields":[]},"schema":{"type":"struct","fields":[{"type":"long","id":1,"name":"x","required":true}],"schema-id":0,"identifier-field-ids":[]}}`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// redirect os.Stdout to test the output of the function
+			oldStdout := os.Stdout
+			r, w, _ := os.Pipe()
+			os.Stdout = w
+			defer func() {
+				os.Stdout = oldStdout
+			}()
 
-	for _, tt := range testArgs {
-		meta, _ := table.ParseMetadataBytes([]byte(tt.meta))
-		table := table.New([]string{"t"}, meta, "", nil, nil)
-		buf.Reset()
+			meta, _ := table.ParseMetadataBytes([]byte(tt.args.meta))
+			tbl := table.New([]string{"t"}, meta, "", nil, nil)
 
-		text{}.DescribeTable(table)
+			jsonOutput{}.DescribeTable(tbl)
 
-		assert.Equal(t, tt.expected, buf.String())
+			w.Close()
+			var buf bytes.Buffer
+			_, _ = buf.ReadFrom(r)
+
+			assert.JSONEq(t, tt.expected, buf.String())
+		})
 	}
 }
