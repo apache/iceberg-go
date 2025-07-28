a28808a51175bfaf077e9e384b9811e9c17211f0
Alex Normand <alex.normand@datadoghq.com>
2025-07-28 15:55:43 -0700

    Bootstrap fork with tooling to make it workable
    Replace hard coded link directive patch with coarse grained link sed

---
+++ initialize-fork.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+if [ ! -d "./go-cloud" ]; then
+    # Update the version if it gets updated upstream
+    git subtree add --prefix=go-cloud git@github.com:google/go-cloud.git v0.43.0
+    # Remove the go module definition to make go-cloud dev a sub-package
+    git rm go-cloud/go.*
+fi
+
+# Delete some workflows because that don't apply to the fork
+git rm .github/workflows/rc.yml
+git rm .github/workflows/license_check.yml
+git rm .github/workflows/go-release-docs.yml
+git rm .github/workflows/labeler.yml
+git rm .github/labeler.yml
+
+# Rename module and update all self-referencing imports 
+go mod edit -module github.com/DataDog/iceberg-go
+go run github.com/sirkon/go-imports-rename@latest --save 'github.com/apache/iceberg-go => github.com/DataDog/iceberg-go'
+
+# Apply a patch for link directives
+find . -name '*.go' -print0 | while IFS= read -r -d '' file; do
+  sed -i '' '/^\/\/go:linkname/ s,github.com/apache/iceberg-go,github.com/DataDog/iceberg-go,g' "$file"
+done
+
+# Replace all gocloud.dev import paths to the inlined subtree of the repo (effectivement vendoring gocloud.dev)
+go run github.com/sirkon/go-imports-rename@latest --save 'gocloud.dev => github.com/DataDog/iceberg-go/go-cloud'
+
+# Tidy up the dependencies
+go mod tidy
