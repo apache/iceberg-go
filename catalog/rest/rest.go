39916cc6f6b00d3ba6ff6a59d9dd2159e83bef3f
Alex Stephen <alexstephen@google.com>
2025-10-07 11:41:33 -0700

    Make PR changes

--- catalog/rest/rest.go
+++ catalog/rest/rest.go
@@ -160,8 +160,6 @@ type createTableRequest struct {
 	Props         iceberg.Properties     `json:"properties,omitempty"`
 }
 
-
-
 type configResponse struct {
 	Defaults  iceberg.Properties `json:"defaults"`
 	Overrides iceberg.Properties `json:"overrides"`
@@ -480,8 +478,6 @@ func (r *Catalog) init(ctx context.Context, ops *options, uri string) error {
 
 	return nil
 }
-
-
 
 func (r *Catalog) createSession(ctx context.Context, opts *options) (*http.Client, error) {
 	session := &sessionTransport{
