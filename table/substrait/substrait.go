3047a2701555fcc5c63168d71b11ccb54919681a
Dao Thanh Tung <ttdao.2015@accountancy.smu.edu.sg>
2025-12-22 15:59:41 +0000

    feat(types): Support Unknown Type for v3 table spec (#605)
    - [x] Write the logic for nested type checking
    - [x] Add more test for the nested field validator
    - [x] move validate unknown type from init() to somewhere like
    checkSchemaCompatibility
    
    ---------
    
    Signed-off-by: dttung2905 <ttdao.2015@accountancy.smu.edu.sg>

--- table/substrait/substrait.go
+++ table/substrait/substrait.go
@@ -164,6 +164,11 @@ func (convertToSubstrait) VisitTimestampNsTz() types.Type {
 func (convertToSubstrait) VisitString() types.Type { return &types.StringType{} }
 func (convertToSubstrait) VisitBinary() types.Type { return &types.BinaryType{} }
 func (convertToSubstrait) VisitUUID() types.Type   { return &types.UUIDType{} }
+func (convertToSubstrait) VisitUnknown() types.Type {
+	// Unknown types cannot be stored in data files and have no Substrait equivalent
+	// Returning nil indicates this type cannot be converted to Substrait
+	return nil
+}
 
 var _ iceberg.SchemaVisitorPerPrimitiveType[types.Type] = (*convertToSubstrait)(nil)
 
