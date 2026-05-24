c0d62b456d9e7c605f5e87ec8e75bb5e3032e41f
Asish Kumar <officialasishkumar@gmail.com>
2026-05-25 04:19:11 +0530

    fix(table): reject reserved metadata IDs before reassignment
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- table/metadata.go
+++ table/metadata.go
@@ -2194,6 +2194,10 @@ func NewMetadataWithUUID(sc *iceberg.Schema, partitions *iceberg.PartitionSpec, 
 		}
 	}
 
+	if err := validateNoReservedMetadataColumnIDs(sc); err != nil {
+		return nil, err
+	}
+
 	reassignedIds, err := reassignIDs(sc, partitions, sortOrder)
 	if err != nil {
 		return nil, err
