ae002d3a39f029dde8a81a0d42753002659a4d21
Minh Vu <vuhoangminh97@gmail.com>
2026-08-08 17:31:25 +0200

    fix(catalog): reject invalid metadata versions

--- catalog/internal/utils.go
+++ catalog/internal/utils.go
@@ -230,17 +230,21 @@ func getDefaultWarehouseLocation(namespaceKey, tablename string, nsprops, catpro
 // (?:\.\w+)?       -> optional codec name
 // \.metadata\.json -> file extension
 var tableMetadataFileNameRegex = regexp.MustCompile(`^(\d+)-([\w-]{36})(?:\.\w+)?\.metadata\.json$`)
+var legacyTableMetadataFileNameRegex = regexp.MustCompile(`^v(\d+)(?:\.gz)?\.metadata\.json$`)
 
 func ParseMetadataVersion(location string) int {
 	fileName := path.Base(location)
 	matches := tableMetadataFileNameRegex.FindStringSubmatch(fileName)
 
-	if len(matches) != 3 {
-		return -1
-	}
-
-	if _, err := uuid.Parse(matches[2]); err != nil {
-		return -1
+	if len(matches) == 3 {
+		if _, err := uuid.Parse(matches[2]); err != nil {
+			return -1
+		}
+	} else {
+		matches = legacyTableMetadataFileNameRegex.FindStringSubmatch(fileName)
+		if len(matches) != 2 {
+			return -1
+		}
 	}
 
 	v, err := strconv.Atoi(matches[1])
@@ -285,7 +289,14 @@ func UpdateAndStageTable(ctx context.Context, catprops iceberg.Properties, curre
 		return nil, err
 	}
 
-	newVersion := ParseMetadataVersion(metadataLoc) + 1
+	newVersion := 0
+	if metadataLoc != "" {
+		metadataVersion := ParseMetadataVersion(metadataLoc)
+		if metadataVersion < 0 {
+			return nil, fmt.Errorf("invalid metadata location: %s", metadataLoc)
+		}
+		newVersion = metadataVersion + 1
+	}
 	newLocation, err := provider.NewTableMetadataFileLocation(newVersion)
 	if err != nil {
 		return nil, err
