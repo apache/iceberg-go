579e8a0a7d626dc34616d3f1fce3f45e8dc037fb
Minh Vu <vuhoangminh97@gmail.com>
2026-08-20 23:43:52 +0200

    fix(table): validate collected position deletes
    Signed-off-by: Minh Vu <vuhoangminh97@gmail.com>

--- table/arrow_scanner_posdelete_regression_test.go
+++ table/arrow_scanner_posdelete_regression_test.go
@@ -603,6 +603,39 @@ func TestCollectPosDeletePositionsRejectsUnsupportedPosType(t *testing.T) {
 	assert.Contains(t, err.Error(), "unsupported pos column type")
 }
 
+func TestCollectPosDeletePositionsRejectsNegativePositions(t *testing.T) {
+	mem := memory.NewCheckedAllocator(memory.DefaultAllocator)
+	defer mem.AssertSize(t, 0)
+
+	posArr := int64Array(mem, -1)
+	defer posArr.Release()
+
+	posCol := arrow.NewChunked(arrow.PrimitiveTypes.Int64, []arrow.Array{posArr})
+	defer posCol.Release()
+
+	_, err := collectPosDeletePositions(positionDeletes{posCol})
+	require.ErrorIs(t, err, iceberg.ErrInvalidSchema)
+	assert.Contains(t, err.Error(), "negative pos -1")
+}
+
+func TestCollectPosDeletePositionsRejectsNullPositions(t *testing.T) {
+	mem := memory.NewCheckedAllocator(memory.DefaultAllocator)
+	defer mem.AssertSize(t, 0)
+
+	bldr := array.NewInt64Builder(mem)
+	bldr.AppendNull()
+	posArr := bldr.NewInt64Array()
+	bldr.Release()
+	defer posArr.Release()
+
+	posCol := arrow.NewChunked(arrow.PrimitiveTypes.Int64, []arrow.Array{posArr})
+	defer posCol.Release()
+
+	_, err := collectPosDeletePositions(positionDeletes{posCol})
+	require.ErrorIs(t, err, iceberg.ErrInvalidSchema)
+	assert.Contains(t, err.Error(), "null pos in position delete file")
+}
+
 func TestReadDeletesRejectsNullPos(t *testing.T) {
 	mem := memory.NewCheckedAllocator(memory.DefaultAllocator)
 	ctx := compute.WithAllocator(t.Context(), mem)
