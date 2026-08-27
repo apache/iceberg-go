267f2775d8da1fd955f998b648c7f9b2115228e6
Minh Vu <vuhoangminh97@gmail.com>
2026-08-27 13:29:10 +0200

    perf(table): skip metrics after partition rejection (#1771)
    * perf(table): skip metrics after partition rejection
    
    Signed-off-by: Minh Vu <vuhoangminh97@gmail.com>
    
    * fix(table): cover manifest filter short-circuit
    
    Signed-off-by: Minh Vu <vuhoangminh97@gmail.com>
    
    ---------
    
    Signed-off-by: Minh Vu <vuhoangminh97@gmail.com>

--- table/scanner.go
+++ table/scanner.go
@@ -206,13 +206,16 @@ func openManifest(io io.IO, manifest iceberg.ManifestFile,
 		if err != nil {
 			return nil, err
 		}
+		if !p {
+			continue
+		}
 
 		m, err := metricsEval(entry.DataFile())
 		if err != nil {
 			return nil, err
 		}
 
-		if p && m {
+		if m {
 			out = append(out, entry)
 		}
 	}
