8e7233b615a2b699772794868dbf0cdeb56a21b4
Minh Vu <vuhoangminh97@gmail.com>
2026-09-23 22:05:06 +0200

    fix(table): trim dynamic partition predicate validation
    Signed-off-by: Minh Vu <vuhoangminh97@gmail.com>

--- partitions.go
+++ partitions.go
@@ -777,7 +777,13 @@ func (ps PartitionSpec) IsUnpartitioned() bool {
 	}
 
 	for _, f := range ps.fields {
-		if _, ok := f.Transform.(VoidTransform); !ok {
+		switch transform := f.Transform.(type) {
+		case VoidTransform:
+		case *VoidTransform:
+			if transform == nil {
+				return false
+			}
+		default:
 			return false
 		}
 	}
